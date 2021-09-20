c6d7250a98a49ea658550061cec5fa13c3c02643
jspw <mhshifat757@gmail.com>
2021-09-20 15:46:21 +0600

    protector added in all secured endpoints

--- api/routes/blog.js
+++ api/routes/blog.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const blogController = require("../controllers/blog");
+const { protector } = require("../middleware/protector");
 
-router.route("/create").post(blogController.postCreateBlog);
+router.route("/create").post(protector, blogController.postCreateBlog);
 router.route("/all").get(blogController.getAllBlogs);
 router.route("/:title").get(blogController.getBlog);
-router.route("/:title").delete(blogController.deleteBlog);
-router.route("/:title").post(blogController.editPost);
+router.route("/:title").delete(protector, blogController.deleteBlog);
+router.route("/:title").post(protector, blogController.editPost);
 module.exports = router;
--- api/routes/comment.js
+++ api/routes/comment.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/comment");
+const { protector } = require("../middleware/protector");
 
-router.route("/create").post(commentController.postCreateComment);
-router.route("/:id").delete(commentController.deleteComment);
-router.route("/:id").post(commentController.editComment);
+router.route("/create").post(protector, commentController.postCreateComment);
+router.route("/:id").delete(protector, commentController.deleteComment);
+router.route("/:id").post(protector, commentController.editComment);
 module.exports = router;
